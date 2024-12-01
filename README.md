ceeb0d14abf320d7de9f10c6954cc713e1036c3c
martin sereinig <ms@veryunited.net>
2024-12-01 16:11:40 +0100

    December 1st (#1)
    part 1, part 2, readme - etc.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# aoc2024
-advent of code '24
+# aoc2024 🎄
+
+[advent of code '24](https://adventofcode.com/). trying rust again, let's see how far i get.
+
+## running this
+
+install rust, etc. - check out the [rust book](https://doc.rust-lang.org/stable/book/) – it'll explain everything nicely. here's some commands to get started. the input files are not checked in, get them from the advent of code [website](https://adventofcode.com/).
+
+- `cargo new dec13`: create a new package (»member«) in the workspace. i'll use this for every single day.
+- `cargo test`: run all the tests.
+- `cargo run`: first navigate in the right package folder, otherwise `input.txt` won't be found.
--- dec01/src/main.rs
+++ dec01/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs;
+
+use dec01::{difference_vector, similarity_score, sum_up_vector};
+
 fn main() {
-    println!("Hello, world!");
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+
+    let input = fs::read_to_string("input1.txt").expect("Failed to read input!");
+    for line in input.lines() {
+        let numbers: Vec<i32> = line
+            .split("   ")
+            .map(|n| n.parse().expect("Not a valid integer!"))
+            .collect();
+
+        left.push(numbers[0]);
+        right.push(numbers[1]);
+    }
+
+    // part 1
+    let diff_vector = difference_vector(&left, &right);
+    let diff_sum = sum_up_vector(&diff_vector);
+    println!("part 1: {:?}", diff_sum);
+
+    // part 2
+    let mut similarity_vector: Vec<i32> = Vec::new();
+    for number in &left {
+        similarity_vector.push(similarity_score(*number, &right))
+    }
+    let similarity_sum = sum_up_vector(&similarity_vector);
+    println!("part 2: {:?}", similarity_sum)
 }
