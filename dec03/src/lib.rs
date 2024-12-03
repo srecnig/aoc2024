04770f6d97d9e701ae43d8419f90a780b8ad64be
martin sereinig <ms@veryunited.net>
2024-12-03 19:18:23 +0100

    now i feel dumb

--- dec03/src/lib.rs
+++ dec03/src/lib.rs
@@ -1,13 +1,15 @@
 use regex::Regex;
 
 pub fn apply_conditionals(haystack: &str) -> String {
-    let proper_haystack = format!("{}{}{}", "do()", haystack, "don't()");
-    let re = Regex::new(r"do\(\).*?don't\(\)").unwrap();
-    let captures: Vec<&str> = re
-        .find_iter(&proper_haystack)
-        .map(|mat| mat.as_str())
-        .collect();
-    captures.join("")
+    let mut proper_haystack = haystack.to_string();
+
+    let re = Regex::new(r"don't\(\).*?do\(\)").unwrap();
+    let captures: Vec<regex::Match> = re.find_iter(haystack).collect();
+    for m in captures.into_iter().rev() {
+        proper_haystack.replace_range(m.start()..m.end(), "");
+    }
+
+    proper_haystack
 }
 
 pub fn detect_multiplications(haystack: &str) -> Vec<String> {
@@ -35,27 +37,16 @@ mod tests {
     #[test]
     fn can_apply_conditionals() {
         assert_eq!(
-            "do()do()1don't()do()3don't()",
-            apply_conditionals(&String::from("do()1don't()2do()3don't()4"))
+            "xmul(2,4)&mul[3,7]!^?mul(8,5))",
+            apply_conditionals(&String::from(
+                "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+            ))
         );
         assert_eq!(
-            "do()1don't()do()3don't()",
-            apply_conditionals(&String::from("1don't()2do()3"))
-        );
-        assert_eq!(
-            "do()1don't()do()3do()4don't()",
-            apply_conditionals(&String::from("1don't()2do()3do()4"))
-        );
-        assert_eq!(
-            "do()1don't()",
-            apply_conditionals(&String::from("1don't()2don't()3don't()4"))
-        );
-        let demo = String::from(
-            "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
-        );
-        assert_eq!(
-            "do()xmul(2,4)&mul[3,7]!^don't()do()?mul(8,5))don't()",
-            apply_conditionals(&demo)
+            String::from("blablablab_1do()2do()3_4567_pepi"),
+            apply_conditionals(&String::from(
+                "blablablab_don't()blablablado()1do()2do()3_4567_don't()hugohugoghuo_do()pepi",
+            ))
         );
     }
 
--- dec03/src/main.rs
+++ dec03/src/main.rs
@@ -4,19 +4,14 @@ use dec03::{apply_conditionals, detect_multiplications, multiply};
 
 fn main() {
     let input = fs::read_to_string("input1.txt").expect("Failed to read input!");
+    let normalized_input = input.replace('\n', "");
 
-    let mut multiplications: Vec<String> = Vec::new();
-    for line in input.lines() {
-        multiplications.extend(detect_multiplications(line));
-    }
+    let multiplications = detect_multiplications(&normalized_input);
     let multiplication_result: i32 = multiplications.iter().map(|m| multiply(m)).sum();
     println!("part1: {:?}", multiplication_result);
 
-    let mut conditional_multiplications: Vec<String> = Vec::new();
-    for line in input.lines() {
-        let conditional_line = apply_conditionals(line);
-        conditional_multiplications.extend(detect_multiplications(&conditional_line));
-    }
+    let conditional_input = apply_conditionals(&normalized_input);
+    let conditional_multiplications = detect_multiplications(&conditional_input);
     let multiplication_result: i32 = conditional_multiplications
         .iter()
         .map(|m| multiply(m))
