f2de0965511fa724a868401fc4966e47e6d1ecfd
martin sereinig <ms@veryunited.net>
2024-12-09 22:49:29 +0100

    December 9th - pt1 (#8)
    this time i don't directly commit to `main`!

---
+++ dec09/src/main.rs
@@ -0,0 +1,12 @@
+use std::fs;
+
+use dec09::Disk;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Failed to read input!");
+    let normalized_input = input.replace('\n', "");
+
+    let mut disk: Disk = Disk::new(&normalized_input);
+    disk.compact();
+    println!("pt1: {}", disk.checksum());
+}
