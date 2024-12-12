5556bb2ce9cf6d1df37f088c7d8c4be9e8f9598d
martin sereinig <ms@veryunited.net>
2024-12-12 12:39:22 +0100

    pt2

--- dec11/src/lib.rs
+++ dec11/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn blink(stones: Vec<i64>) -> Vec<i64> {
     let mut new_stones: Vec<i64> = Vec::new();
     for stone in stones {
@@ -17,6 +19,55 @@ pub fn blink(stones: Vec<i64>) -> Vec<i64> {
         }
     }
     new_stones
+}
+
+pub fn blink_efficiently(initial_stones: &Vec<i64>, blink_count: i32) -> i64 {
+    let mut stones: HashMap<i64, i64> = HashMap::new();
+    for stone in initial_stones {
+        stones.insert(*stone, 1);
+    }
+
+    for _ in 0..blink_count {
+        let mut new_stones: HashMap<i64, i64> = HashMap::new();
+        let stone_values: Vec<i64> = stones.keys().cloned().collect();
+
+        for stone_value in stone_values {
+            let current_count = stones.remove(&stone_value).unwrap();
+
+            if stone_value == 0 {
+                // 0 turns into 1
+                let new_value: i64 = 1;
+                new_stones
+                    .entry(new_value)
+                    .and_modify(|e| *e += current_count)
+                    .or_insert(current_count);
+            } else {
+                let digits = stone_value.to_string();
+                let digit_count = digits.len();
+                if digit_count % 2 == 0 {
+                    let left: i64 = digits[0..digit_count / 2].parse().unwrap();
+                    new_stones
+                        .entry(left)
+                        .and_modify(|e| *e += current_count)
+                        .or_insert(current_count);
+
+                    let right: i64 = digits[digit_count / 2..].parse().unwrap();
+                    new_stones
+                        .entry(right)
+                        .and_modify(|e| *e += current_count)
+                        .or_insert(current_count);
+                } else {
+                    let new_value = stone_value * 2024;
+                    new_stones
+                        .entry(new_value)
+                        .and_modify(|e| *e += current_count)
+                        .or_insert(current_count);
+                }
+            };
+        }
+        stones = new_stones;
+    }
+    stones.values().sum()
 }
 
 #[cfg(test)]
@@ -49,5 +100,12 @@ mod tests {
         let mut stones: Vec<i64> = vec![125, 17];
         stones = blink(stones);
         assert_eq!(vec![253000, 1, 7], stones);
+    }
+
+    #[test]
+    fn can_blink_efficiently() {
+        let stones: Vec<i64> = vec![125, 17];
+        assert_eq!(9, blink_efficiently(&stones, 4));
+        assert_eq!(22, blink_efficiently(&stones, 6));
     }
 }
--- dec11/src/main.rs
+++ dec11/src/main.rs
@@ -1,24 +1,19 @@
 use std::fs;
 
-use dec11::blink;
+use dec11::blink_efficiently;
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Failed to read input!");
     let normalized_input = input.replace('\n', "");
 
-    let mut stones: Vec<i64> = normalized_input
+    let stones: Vec<i64> = normalized_input
         .split(" ")
         .map(|n| n.parse().unwrap())
         .collect();
 
-    for _ in 0..25 {
-        stones = blink(stones);
-    }
-    println!("pt1: {:?}", stones.len());
+    let stone_count = blink_efficiently(&stones, 25);
+    println!("pt1: {:?}", &stone_count);
 
-    for i in 0..50 {
-        stones = blink(stones);
-        println!("{}", i);
-    }
-    println!("pt2: {:?}", stones.len());
+    let stone_count = blink_efficiently(&stones, 75);
+    println!("pt2: {:?}", &stone_count);
 }
